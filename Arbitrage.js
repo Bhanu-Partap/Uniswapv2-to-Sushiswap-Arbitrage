8f892217b6f291e5885a3dbc66594000186883d0
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-09 11:04:13 +0530

    pair token & router address selected

--- Arbitrage.js
+++ Arbitrage.js
@@ -1,8 +1,11 @@
-require("dotenv").config()  
-const ethers = require("ethers")
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+require("dotenv").config();
+const ethers = require("ethers");
+const [RPC_URL, TOKEN0, TOKEN1, UNIROUTERADDRESS, SUSHIROUTERADDRESS] =
+  proces.env;
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
-const token0 = process.env.token0
-const token1 = process.env.token1
+const token0 = TOKEN0;
+const token1 = TOKEN1;
 
-
+const uniRouterAddress = UNIROUTERADDRESS;
+const sushiRouterAddress = SUSHIROUTERADDRESS;
