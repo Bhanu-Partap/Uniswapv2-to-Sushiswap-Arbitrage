5f35b9481d64d2a287ed5da6f86b2edb26c5144f
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-09 10:52:52 +0530

    pair token selected

--- Arbitrage.js
+++ Arbitrage.js
@@ -1,3 +1,8 @@
-require("dotenv").config()
+require("dotenv").config()  
 const ethers = require("ethers")
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+
+const token0 = process.env.token0
+const token1 = process.env.token1
+
+
