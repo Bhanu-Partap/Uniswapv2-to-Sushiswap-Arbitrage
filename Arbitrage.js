c1d686440823a82763e04cf4ebf4a828dc4404f0
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-09 10:43:39 +0530

    Uni to Sushi arbitrage

--- Arbitrage.js
+++ Arbitrage.js
@@ -1,2 +1,3 @@
+require("dotenv").config()
 const ethers = require("ethers")
-const provider = new ethers.providers.JsonRpcProvider("")
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
